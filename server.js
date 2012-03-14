9145782f8d10805b8bb0de359833f208ad161a35
Shuo Zhang <szhang@unca.edu>
2012-03-14 10:37:19 -0400

    Modified to follow all words

--- server.js
+++ server.js
@@ -4,10 +4,12 @@ var redis = require('redis');
 var client = redis.createClient();
 
 http.createServer(function (req, res) {
-	client.mget(['awesome', 'gnarly'], function(error, responses) {
+	client.mget(['awesome', 'gnarly', 'cool', 'rad', 'groovy'], function(error, responses) {
 		console.log(responses);
 		res.writeHead(200, {'Content-Type': 'text/html'});
-		res.end('awesomeCount: ' + responses[0] + '<br/> gnarlyCount:' + responses[1]);		
+		res.end('awesomeCount: ' + responses[0] + '<br/> gnarlyCount:' + responses[1] +
+		'<br/> coolCount:' + responses[2] +  '<br/> radCount:' + responses[3] +
+		'<br/> groovyCount:' + responses[4]);		
 	});
 }).listen(3000);
 
